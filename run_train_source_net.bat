33a53adbafd60fe23d0928ca99d49df9937799f3
MHong1001 <chuamh_1001@hotmail.com>
2021-01-20 17:22:04 +0800

    update bat file of training source networks

--- run_train_source_net.bat
+++ run_train_source_net.bat
@@ -1,12 +1,21 @@
 call .venv/Scripts/activate
 
-python train_source_networks.py --dataset=first5_mnist &
-python train_source_networks.py --dataset=last5_mnist &
-python train_source_networks.py --dataset=mnist &
-python train_source_networks.py --dataset=cifar10 --batch_size=128 &
+python train_source_networks.py --dataset=first5_mnist --arch=resnet18
+python train_source_networks.py --dataset=last5_mnist --arch=resnet18
 
-python train_source_networks.py --dataset=mnist --arch=resnet18 --lr=0.1 &
-python train_source_networks.py --dataset=cifar10 --arch=resnet18 --batch_size=128 --lr=0.1 --epochs 20 
+python train_source_networks.py --dataset=mnist --arch=resnet18
+python train_source_networks.py --dataset=cifar10 --arch=resnet18
+
+python train_source_networks.py --dataset=fmnist --arch=resnet18
+python train_source_networks.py --dataset=kmnist --arch=resnet18
+
+REM python train_source_networks.py --dataset=first5_mnist &
+REM python train_source_networks.py --dataset=last5_mnist &
+REM python train_source_networks.py --dataset=mnist &
+REM python train_source_networks.py --dataset=cifar10 --batch_size=128 &
+
+REM python train_source_networks.py --dataset=mnist --arch=resnet18 --lr=0.1 &
+REM python train_source_networks.py --dataset=cifar10 --arch=resnet18 --batch_size=128 --lr=0.1 --epochs 20 
 
 REM python train_source_networks.py --dataset=first5_mnist --arch=lenet5_halfed &
 REM python train_source_networks.py --dataset=last5_mnist --arch=lenet5_halfed &
