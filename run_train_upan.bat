a2a5da3948a7ac1d186b75c55b0443b544ffb3ea
MHong1001 <chuamh_1001@hotmail.com>
2021-01-25 02:41:44 +0800

    update train upan batch file

--- run_train_upan.bat
+++ run_train_upan.bat
@@ -1,13 +1,13 @@
 call .venv/Scripts/activate
 
-python train_upan.py --dataset=disjoint_mnist –-testset=disjoint_mnist --upan_type=logits &
-python train_upan.py --dataset=mnist_cifar10 –-testset=mnist_cifar10 --upan_type=logits &
-python train_upan.py --dataset=mnist_cifar10 –-testset=fmnist_kmnist --upan_type=logits &
+python train_upan.py --dataset=disjoint_mnist --testset=disjoint_mnist --upan_type=logits &
+python train_upan.py --dataset=mnist_cifar10 --testset=mnist_cifar10 --upan_type=logits &
+python train_upan.py --dataset=mnist_cifar10 --testset=fmnist_kmnist --upan_type=logits &
 
-python train_upan.py --dataset=disjoint_mnist –-testset=disjoint_mnist --upan_type=agnostic_logits &
-python train_upan.py --dataset=disjoint_mnist –-testset=mnist_cifar10 --upan_type=agnostic_logits &
-python train_upan.py --dataset=disjoint_mnist –-testset=fmnist_kmnist --upan_type=agnostic_logits &
+python train_upan.py --dataset=disjoint_mnist --testset=disjoint_mnist --upan_type=agnostic_logits &
+python train_upan.py --dataset=disjoint_mnist --testset=mnist_cifar10 --upan_type=agnostic_logits &
+python train_upan.py --dataset=disjoint_mnist --testset=fmnist_kmnist --upan_type=agnostic_logits &
 
-python train_upan.py --dataset=mnist_cifar10 –-testset=disjoint_mnist --upan_type=agnostic_logits &
-python train_upan.py --dataset=mnist_cifar10 –-testset=mnist_cifar10 --upan_type=agnostic_logits &
-python train_upan.py --dataset=mnist_cifar10 –-testset=fmnist_kmnist --upan_type=agnostic_logits
+python train_upan.py --dataset=mnist_cifar10 --testset=disjoint_mnist --upan_type=agnostic_logits &
+python train_upan.py --dataset=mnist_cifar10 --testset=mnist_cifar10 --upan_type=agnostic_logits &
+python train_upan.py --dataset=mnist_cifar10 --testset=fmnist_kmnist --upan_type=agnostic_logits
